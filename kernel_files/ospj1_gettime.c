956bba3d848fddc85228f37bd73cff5cce652575
kaienlin <kaienlin1999@gmail.com>
2020-04-25 03:00:56 +0800

    Migrate to Ubuntu 16.04/Linux Kernel 4.14.25 and change the api

--- kernel_files/ospj1_gettime.c
+++ kernel_files/ospj1_gettime.c
@@ -2,6 +2,9 @@
 #include <linux/linkage.h>
 #include <linux/timer.h>
 
-asmlinkage void sys_ospj1_gettime(struct timespec *ts) {
-        getnstimeofday(ts);
+asmlinkage void sys_ospj1_gettime(long *sec, long *nsec) {
+        struct timespec ts;
+        getnstimeofday(&ts);
+        *sec = ts.tv_sec;
+        *nsec = ts.tv_nsec;
 }
--- kernel_files/ospj1_showinfo.c
+++ kernel_files/ospj1_showinfo.c
@@ -1,7 +1,6 @@
 #include <linux/kernel.h>
 #include <linux/linkage.h>
-#include <linux/timer.h>
 
-asmlinkage void sys_showinfo(int pid, long st_sec, long st_nsec, long ft_sec, long ft_nsec) {
-        printk(KERN_INFO "[Project1] %d %.9ld.%.9ld %.9ld.%.9ld\n", st_sec, st_nsec, ft_sec, ft_nsec);
+asmlinkage void sys_ospj1_showinfo(pid_t pid, long st_sec, long st_nsec, long ft_sec, long ft_nsec) {
+        printk(KERN_INFO "[Project1] %d %ld.%.9ld %ld.%.9ld\n", pid, st_sec, st_nsec, ft_sec, ft_nsec);
 }
